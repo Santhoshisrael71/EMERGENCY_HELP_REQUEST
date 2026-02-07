2f2e27d7d84cbd86d68eb9556adaf69ac075279e
Santhoshisrael71 <santhoshisrael71@gmail.com>
2026-02-07 09:57:16 +0530

    success

--- server.py
+++ server.py
@@ -151,6 +151,4 @@ def approve(alert_id):
     return redirect(url_for('dashboard'))
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
